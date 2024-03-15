618de07219a5a6f971749fc553d3ac42ec86f1f6
Eric w <none>
2024-03-14 18:07:30 -0700

    added better error handling

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 const static = require('./routes/static');
 const inventoryRoute = require('./routes/inventoryRoute');
 const baseController = require('./controllers/baseController');
+const Util = require('./utilities');
 
 /* ***********************
  * View Engine and Templates
@@ -26,9 +27,28 @@ app.set('layout', './layouts/layout');
  *************************/
 app.use(static);
 // Index Route
-app.get('/', baseController.buildHome);
+app.get('/', Util.handleErrors(baseController.buildHome));
 // Inventory routes
 app.use('/inv', inventoryRoute);
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+    let nav = await utilities.getNav();
+    console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+    if (err.status == 404) {
+        message = err.message;
+    } else {
+        message = 'Oh no! There was a crash. Maybe try a different route?';
+    }
+    res.render('errors/error', {
+        title: err.status || 'Server Error',
+        message,
+        nav,
+    });
+});
 
 /* ***********************
  * Local Server Information
